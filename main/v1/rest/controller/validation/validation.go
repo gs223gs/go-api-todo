23f0c38ad2ee8773770e71f83c327655e7bbefc9
T.MiuraPro <mtaiga0213@icloud.com>
2025-03-14 21:34:36 +0900

    feat: バリデーションチェック関数の作成開始

---
+++ main/v1/rest/controller/validation/validation.go
@@ -0,0 +1,68 @@
+package validation
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+//! package名がvalidationのため，関数名をわざと短くしている
+func TodoID() {
+
+}
+
+func CategoryID(db *gorm.DB, id int) {
+
+}
+
+func ContentType() {
+
+}
+
+func TodoTitle(Title string) error {
+	if Title == "" {
+		return fmt.Errorf("Todo名がありません")
+	}
+	return nil
+}
+
+func Check(s []string) (result map[string]error) {
+	result = make(map[string]error)
+	for _,v := range s{
+		switch v {
+		case "TodoId":
+		case "TodoTitle":
+			if err := TodoTitle(v); err != nil{
+				result["TodoTitle"] = err
+			}
+		case "CategoryId":
+		case "Content-Type":
+		default:
+			// デフォルトの処理をここに追加
+		}
+	}
+	return result
+}
+
+/*
+check(s スライス ) (map[string]stringerr) {
+result map[string]string
+swich
+	TodoID
+		if check {
+		,todoid :="TodoID"
+			err := checkfunc(TodoID); err != nil{
+			result[todoid] = err
+			}
+		}
+	CategoryID
+		if check{
+			return err
+		}
+	ContentType
+		if check{
+			return err
+		}
+	return result
+}
+*/
