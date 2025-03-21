e669c6e66005a6c73c639ef4be881adec5db3e5d
T.MiuraPro <mtaiga0213@icloud.com>
2025-03-21 22:45:10 +0900

    fix: updatedが正常に動作するように修正
    gormの機能でcreated_at, updated_atは特別なもので，明示的に更新する必要はないらしい

--- main/v1/rest/controller/todo/todo.go
+++ main/v1/rest/controller/todo/todo.go
@@ -5,15 +5,15 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gs223gs/go-webapi-todo/controller/validation"
-	"github.com/gs223gs/go-webapi-todo/structs"
+	"github.com/gs223gs/go-api-todo/controller/validation"
+	"github.com/gs223gs/go-api-todo/structs"
 	"gorm.io/gorm"
 )
 
 func V1RestTodo(r *gin.Engine, db *gorm.DB) {
 	r.GET("/v1/rest/todo", func(c *gin.Context) {
 		var todos []structs.Todos
-		db.Select("Id", "Title", "Content", "Category_Id", "Due", "Is_Done", "Created_at").Find(&todos)
+		db.Select("Id", "Title", "Content", "Category_Id", "Due", "Is_Done", "Created_at", "Updated_at").Find(&todos)
 
 		var todosResponse []structs.TodosResponse
 		for _, todo := range todos {
@@ -61,39 +61,39 @@ func V1RestTodo(r *gin.Engine, db *gorm.DB) {
 
 	//REST原則に基づいて PUTを実装し直す
 	r.PUT("/v1/rest/todo", func(c *gin.Context) {
-		var todo structs.Todos
-		if err := c.ShouldBindJSON(&todo); err != nil {
+		var updateTodo structs.Todos
+		if err := c.ShouldBindJSON(&updateTodo); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		var validate = map[string]any{"TodoID": todo.Id, "TodoTitle": todo.Title, "CategoryID": todo.Category_Id}
+		var validate = map[string]any{"TodoID": updateTodo.Id, "TodoTitle": updateTodo.Title, "CategoryID": updateTodo.Category_Id}
 		if err := validation.Check(validate, db); len(err) != 0 {
 			c.JSON(http.StatusBadRequest, gin.H(validation.Conv(err)))
 			return
 		}
 
+		// 既存のレコードを取得
 		var existingTodo structs.Todos
-		if err := db.First(&existingTodo, todo.Id).Error; err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Todoが見つかりません"})
+		if err := db.First(&existingTodo, updateTodo.Id).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "指定されたTodoが見つかりません"})
 			return
 		}
 
-		timenow := time.Now()
-
-		existingTodo.Title = todo.Title
-		existingTodo.Content = todo.Content
-		existingTodo.Category_Id = todo.Category_Id
-		existingTodo.Is_Done = todo.Is_Done
-		existingTodo.Due = todo.Due
-		existingTodo.Updated_at = timenow
+		// 更新したいフィールドのみを更新
+		existingTodo.Title = updateTodo.Title
+		existingTodo.Content = updateTodo.Content
+		existingTodo.Category_Id = updateTodo.Category_Id
+		existingTodo.Is_Done = updateTodo.Is_Done
+		existingTodo.Due = updateTodo.Due
+		existingTodo.Updated_at = time.Now() // 更新時刻を設定
 
 		if err := db.Save(&existingTodo).Error; err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "更新に失敗しました"})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"messege": "更新完了"})
+		c.JSON(http.StatusOK, gin.H{"message": "更新完了"})
 	})
 
 	r.DELETE("/v1/rest/todo", func(c *gin.Context) {
