ccb816500a841f94886db4ea6b01a433b65b71f5
T.MiuraPro <mtaiga0213@icloud.com>
2025-04-04 22:02:21 +0900

    experiment: interfaceの勉強

--- rest-jwt/backend/main.go
+++ rest-jwt/backend/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 )
-//response
+
+// response
 type Todos struct {
 	Todo []TodoResponse `json:"todos"`
 }
 type TodoResponse struct {
-	ID int `json:"id"`
-	Title string `json:"title"`
-	Completed bool `json:"completed"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
 }
-
 
 type ErrResponse struct {
 	Message map[string]any `json:"message"`
@@ -22,9 +22,7 @@ type TodoMethod interface {
 	CheckTitle() error
 }
 
-
-
-//request
+// request
 type TodoCreateRequest struct {
 	Title string `json:"title"`
 }
@@ -37,25 +35,47 @@ func (t TodoCreateRequest) CheckTitle() error {
 }
 
 type TodoUpdateRequest struct {
-	Id int `json:"id"`
-	Title string `json:"title"`
-	Completed bool `json:"completed"`
+	Id        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
 }
 
-//gorm db mygrate
+func (t TodoUpdateRequest) CheckTitle() error {
+	if t.Title == "" || len(t.Title) > 255 {
+		return fmt.Errorf("Todo Titleがないか、255文字以上です")
+	}
+	return nil
+}
+
+func (t TodoUpdateRequest) CheckId() error {
+	if t.Id <= 0 {
+		return fmt.Errorf("Todo IDがないか、0以下です")
+	}
+	return nil
+}
+
+// gorm db mygrate
 type TodoTable struct {
-	ID int `gorm:"primary_key;auto_increment"`
-	Title string `gorm:"size:255"`
-	Completed bool `gorm:"default:false"`
+	ID        int    `gorm:"primary_key;auto_increment"`
+	Title     string `gorm:"size:255"`
+	Completed bool   `gorm:"default:false"`
 }
 
 func main() {
-	var CreateRequest TodoMethod = &TodoCreateRequest{"test"}
+	var CreateRequest TodoMethod = &TodoCreateRequest{""}
+	// var UpdateRequest TodoMethod = &TodoUpdateRequest{1, "a", false}
+	UpdateRequest := TodoUpdateRequest{0, "", false}
 	if err := CreateRequest.CheckTitle(); err != nil {
 		fmt.Println(err)
 	}
-
+	if err := UpdateRequest.CheckTitle(); err != nil {
+		fmt.Println(err)
+	}
+	if err := UpdateRequest.CheckId(); err != nil {
+		fmt.Println(err)
+	}
 }
+
 /*
 Todo
 DB-struct
