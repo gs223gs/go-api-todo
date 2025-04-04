ba091f699fff9258b46702a4345deff7bd750cfd
T.MiuraPro <mtaiga0213@icloud.com>
2025-04-04 21:11:58 +0900

    feat: request / response の structを定義 Todoリストを記述

--- rest-jwt/backend/main.go
+++ rest-jwt/backend/main.go
@@ -3,7 +3,58 @@ package main
 import (
 	"fmt"
 )
+//response
+type Todos struct {
+	Todo []TodoResponse `json:"todos"`
+}
+type TodoResponse struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Completed bool `json:"completed"`
+}
+
+//request
+type TodoCreateRequest struct {
+	Title string `json:"title"`
+}
+
+type TodoUpdateRequest struct {
+	Id int `json:"id"`
+	Title string `json:"title"`
+	Completed bool `json:"completed"`
+}
+
+//gorm db mygrate
+type TodoTable struct {
+	ID int `gorm:"primary_key;auto_increment"`
+	Title string `gorm:"size:255"`
+	Completed bool `gorm:"default:false"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	
 }
+/*
+Todo
+DB-struct
+gorm
+gin
+JWT
+api-end-point{
+	/login[
+		POST
+	]
+
+	/signup[
+		POST
+	]
+
+	/todo[
+		GET
+		POST
+		PUT
+		DELETE
+	]
+}
+
+*/
