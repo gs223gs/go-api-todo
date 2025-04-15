478cbf8be68b410fe9058fadeadd05c02478b4b7
T.Miura <mtaiga0213@icloud.com>
2025-04-15 21:14:10 +0900

    feat: register画面の実装
    registe ->
    login ->
    todo
    
    の流れで画面遷移

--- rest-jwt/frontend/jwt-todo/src/App.jsx
+++ rest-jwt/frontend/jwt-todo/src/App.jsx
@@ -1,19 +1,33 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const handleSubmit = (e) => {
+  const handleLoginSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
     setUsername("");
     setPassword("");
+    setIsLogin(false);
+  };
+  const handleRegisterSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    setUsername("");
+    setPassword("");
+    setIsRegister(false);
     setIsLogin(true);
   };
   const handleTodoSubmit = (e) => {
     e.preventDefault();
   };
+
+  const handleRegister = () => {
+    setIsRegister(!isRegister);
+    setIsLogin(!isLogin);
+  };
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useState(false);
+  const [isRegister, setIsRegister] = useState(true);
   //todolist
   //構造体 id todo isdone
   const todolist = [
@@ -23,33 +37,38 @@ function App() {
   ];
   const [todoList, setTodoList] = useState(todolist);
   return (
-    <>
-      {isLogin ? (
+    <div>
+      <button onClick={handleRegister}>
+        {isRegister ? "login" : "register"}
+      </button>
+      {isRegister ? (
         <div>
-          <button onClick={() => setIsLogin(false)}>logout</button>
+          <p>register</p>
           <form action="">
-            <span>todo名 : </span>
-            <input type="text"  />
+            <span>username : </span>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
             <br />
-            <button type="submit"  onClick={handleTodoSubmit}>
-              Todo作成
+            <span>password : </span>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <button type="submit" onClick={handleRegisterSubmit}>
+              Create
             </button>
-            <br />
           </form>
-
-          <div>
-            {todoList.map((todo) => (
-              <div key={todo.id}>
-                {todo.todo} : {todo.isdone ? "完了" : "未完了"}
-              </div>
-            ))}
-          </div>
-
         </div>
-      ) : (
+      ) : isLogin ? (
         <form action="">
+          <p>login</p>
           <span>username : </span>
-          <input 
+          <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
@@ -62,12 +81,33 @@ function App() {
             onChange={(e) => setPassword(e.target.value)}
           />
           <br />
-          <button type="submit" onClick={handleSubmit}>
+          <button type="submit" onClick={handleLoginSubmit}>
             Create
           </button>
         </form>
+      ) : (
+        <div>
+          <button onClick={() => setIsLogin(false)}>logout</button>
+          <form action="">
+            <span>todo名 : </span>
+            <input type="text" />
+            <br />
+            <button type="submit" onClick={handleTodoSubmit}>
+              Todo作成
+            </button>
+            <br />
+          </form>
+
+          <div>
+            {todoList.map((todo) => (
+              <div key={todo.id}>
+                {todo.todo} : {todo.isdone ? "完了" : "未完了"}
+              </div>
+            ))}
+          </div>
+        </div>
       )}
-    </>
+    </div>
   );
 }
 
